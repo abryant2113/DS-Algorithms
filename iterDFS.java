b60c713750d501b6973967ae697fe3989e93b356
Austin Bryant <abryant2113@knights.ucf.edu>
2017-08-23 23:04:54 -0400

    iterDFS.java

---
+++ iterDFS.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+
+
+public class iterDFS
+{
+
+
+	public static void main(String[] args) throws Exception
+	{
+		//blahhhh
+		dfs();
+	}
+
+
+
+	public static void dfs() throws Exception
+	{
+	
+		Scanner in = new Scanner(new File("amatrix.txt"));
+	
+		//stuff goes here
+		int n = in.nextInt();
+		
+		boolean [] visited = new boolean[n];
+
+		System.out.println(visited[0]);
+
+	}
+}
